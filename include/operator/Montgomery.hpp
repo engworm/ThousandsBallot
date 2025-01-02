8f597c734fa6bb62bf2b76255f154b2661780d4a
engw <morita_1247@yahoo.co.jp>
2025-01-02 21:56:43 +0900

    Separate header file torus.hpp galoisfield.hpp

--- include/operator/Montgomery.hpp
+++ include/operator/Montgomery.hpp
@@ -6,48 +6,12 @@
 #include <bit>
 #include "params/params.hpp"
 
-std::pair<int32_t, int32_t> extendedEuclidean(uint32_t a, uint32_t b) {
-  if (b==0) {
-    return std::pair<int32_t, int32_t> (1, 0);
-  }
-  std::pair<int32_t, int32_t> r = extendedEuclidean(b, a%b);
-  int32_t s = r.first;
-  int32_t t = r.second;
-  return std::pair<int32_t, int32_t> (t, s - t*(a/b));
-}
-
-// Compute μ = -P^{-1} mod R
-uint32_t constMontgomeryMu() {
-  int32_t r = extendedEuclidean(MontgomeryParams::R, Params::P).second;
-  return (uint32_t)(MontgomeryParams::R - r);
-}
-
-uint32_t constMontgomeryR2() {
-  uint64_t r2 = ((uint64_t)MontgomeryParams::R * MontgomeryParams::R);
-  return r2 % Params::P;
-}
-
-uint32_t redcMontgomery(uint64_t x) {
-  uint32_t q = MontgomeryParams::mu*(x & (MontgomeryParams::R-1)) & (MontgomeryParams::R-1); // mu*(x%R)%R;
-  uint32_t r = (x + Params::P*q) >> std::countr_zero(MontgomeryParams::R);
-  if (r >= Params::P) {
-    r -= Params::P;
-  }
-  return r;
-}
-
-uint32_t mulMontgomery(uint32_t X, uint32_t Y) {
-  uint32_t r = redcMontgomery((uint64_t)X*Y);
-  return r;
-}
-
-uint32_t reprMontgomery(uint32_t x) {
-  return mulMontgomery(x, MontgomeryParams::R2);
-}
-
-uint32_t invReprMontgomery(uint32_t X) {
-  return mulMontgomery(X, 1);
-}
-
+std::pair<int32_t, int32_t> extendedEuclidean(uint32_t a, uint32_t b);
+uint32_t constMontgomeryMu();
+uint32_t constMontgomeryR2();
+uint32_t redcMontgomery(uint64_t x);
+uint32_t mulMontgomery(uint32_t X, uint32_t Y);
+uint32_t reprMontgomery(uint32_t x);
+uint32_t invReprMontgomery(uint32_t X);
 
 #endif
--- include/structure/galoisfield.hpp
+++ include/structure/galoisfield.hpp
@@ -20,67 +20,26 @@ namespace galoisfield {
 
     public:
       DiscreteTorus();
-      DiscreteTorus(uint32_t x) : x(x) {
-          if (x >= this->P) {
-            throw std::invalid_argument("Discrete Torus Element must be less than P");
-          }
-        }
+      DiscreteTorus(uint32_t x);
 
       DiscreteTorus(const DiscreteTorus &t);
       DiscreteTorus(const GaloisFieldElement &a);
 
-      uint32_t val() const {
-        return this->x;
-      };
+      uint32_t val() const;
 
-      void operator+=(const DiscreteTorus &t) {
-        uint32_t tmp = this->x + t.x;
-        this->x = modP(tmp);
-        return;
-      };
-
-      void operator-=(const DiscreteTorus &t) {
-        uint32_t tmp = this->x + (this->P - t.x);
-        this->x = modP(tmp);
-        return;
-      };
-
-      void operator*=(const uint32_t c) {
-        uint32_t X = reprMontgomery(this->x);
-        uint32_t C = reprMontgomery(c);
-        this->x = invReprMontgomery(mulMontgomery(X, C));
-        return;
-      };
+      void operator+=(const DiscreteTorus &t);
+      void operator-=(const DiscreteTorus &t);
+      void operator*=(const uint32_t c);
 
       // should replace it to faster algo
-      uint32_t modP(uint32_t x) {
-        return x%this->P;
-      }
+      uint32_t modP(uint32_t x);
 
-      friend std::ostream& operator<<(std::ostream &os, const DiscreteTorus &t) {
-        os << t.x;
-        return os;
-      }
+      friend std::ostream& operator<<(std::ostream &os, const DiscreteTorus &t);
 
   };
 
-  DiscreteTorus::DiscreteTorus(const DiscreteTorus &t) {
-    this->x = t.x;
-    this->P = t.P;
-    return;
-  }
-
-  DiscreteTorus operator+(const DiscreteTorus &t1, const DiscreteTorus &t2) {
-    DiscreteTorus t = t1;
-    t += t2;
-    return t;
-  }
-
-  DiscreteTorus operator*(const uint32_t c, const DiscreteTorus &t1) {
-    DiscreteTorus t = t1; 
-    t *= c;
-    return t;
-  }
+  DiscreteTorus operator+(const DiscreteTorus &t1, const DiscreteTorus &t2);
+  DiscreteTorus operator*(const uint32_t c, const DiscreteTorus &t1);
 
 }
 
@@ -89,82 +48,32 @@ class GaloisFieldElement {
     uint32_t a{};
     uint32_t P = Params::P;
   
-    uint32_t modP(uint32_t x) {
-      return x%this->P;
-    }
+    uint32_t modP(uint32_t x);
 
   public:
-    GaloisFieldElement() : a(0) {};
+    GaloisFieldElement(); 
 
-    GaloisFieldElement(const GaloisFieldElement &a) : a(a.a) {}; 
-    GaloisFieldElement(const uint32_t &x) : a(x) {}; 
-    GaloisFieldElement(const galoisfield::DiscreteTorus &t) : a(t.val()) {}; 
+    GaloisFieldElement(const GaloisFieldElement &a);
+    GaloisFieldElement(const uint32_t &x);
+    GaloisFieldElement(const galoisfield::DiscreteTorus &t);
 
-    uint32_t val() const {
-      return this->a;
-    }
+    uint32_t val() const;
 
-    void operator+=(const GaloisFieldElement &b) {
-      uint32_t tmp = this->a + b.a;
-      this->a = modP(tmp);
-      return;
-    };  
+    void operator+=(const GaloisFieldElement &b);
+    void operator-=(const GaloisFieldElement &a);
+    void operator*=(const GaloisFieldElement &b);
 
-    void operator-=(const GaloisFieldElement &a) {
-      uint32_t tmp = this->a + (this->P - a.a);
-      this->a = modP(tmp);
-      return;
-    };
+    GaloisFieldElement& operator=(const GaloisFieldElement& other);
 
-    void operator*=(const GaloisFieldElement &b) {
-      uint32_t A = reprMontgomery(this->a);
-      uint32_t B = reprMontgomery(b.a);
-      this->a = invReprMontgomery(mulMontgomery(A, B));
-      return;
-    };
-
-    GaloisFieldElement& operator=(const GaloisFieldElement& other) {
-      if (this != &other) {
-        this->a = other.a;
-        this->P = other.P;
-      }
-      return *this;
-    }
-
-    GaloisFieldElement& operator=(GaloisFieldElement&& other) noexcept {
-      if (this != &other) {
-        this->a = other.a;
-        this->P = other.P;
-        other.a = 0;
-        other.P = 0;
-      }
-      return *this;
-    }
+    GaloisFieldElement& operator=(GaloisFieldElement&& other) noexcept;
 
 
-    friend std::ostream& operator<<(std::ostream &os, const GaloisFieldElement &a) {
-      os << a.a;
-      return os;
-    }
+    friend std::ostream& operator<<(std::ostream &os, const GaloisFieldElement &a);
 };
 
-bool operator==(const GaloisFieldElement &a1, const GaloisFieldElement&a2) {
-  return a1.val() == a2.val();
-}
-
-GaloisFieldElement operator+(const GaloisFieldElement &a, const GaloisFieldElement &b) {
-  GaloisFieldElement c = a;
-  c += b;
-  return c;
-}
-
-GaloisFieldElement operator*(const GaloisFieldElement a, const GaloisFieldElement &b) {
-  GaloisFieldElement c = a; 
-  c *= b;
-  return c;
-}
-
-galoisfield::DiscreteTorus::DiscreteTorus(const GaloisFieldElement &a) : x(a.val()) {};
+bool operator==(const GaloisFieldElement &a1, const GaloisFieldElement&a2);
+GaloisFieldElement operator+(const GaloisFieldElement &a, const GaloisFieldElement &b);
+GaloisFieldElement operator*(const GaloisFieldElement a, const GaloisFieldElement &b);
 
 
 #endif
