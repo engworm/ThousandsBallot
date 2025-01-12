c7bb5f7f2a8683d90fd4253c81a55849a5655a33
engw <morita_1247@yahoo.co.jp>
2025-01-12 14:50:43 +0900

    Separate torus class and galoisfield class by using forward declaration

--- include/structure/torus.hpp
+++ include/structure/torus.hpp
@@ -3,6 +3,39 @@
 
 #include "structure/galoisfield.hpp"
 
-using DiscreteTorus = galoisfield::DiscreteTorus;
+class GaloisFieldElement;
+
+class DiscreteTorus {
+friend class TorusPoly;
+
+private:
+    uint32_t x;
+    uint32_t P = Params::P;
+
+public:
+    DiscreteTorus();
+    DiscreteTorus(uint32_t x);
+
+    // DiscreteTorus(const uint32_t &x); 
+    DiscreteTorus(const DiscreteTorus &t);
+    DiscreteTorus(const GaloisFieldElement &a);
+
+    // DiscreteTorus(uint32_t &&x) noexcept;
+
+    uint32_t val() const;
+
+    void operator+=(const DiscreteTorus &t);
+    void operator-=(const DiscreteTorus &t);
+    void operator*=(const uint32_t c);
+
+    // should replace it to faster algo
+    uint32_t modP(uint32_t x);
+
+    friend std::ostream& operator<<(std::ostream &os, const DiscreteTorus &t);
+
+};
+
+DiscreteTorus operator+(const DiscreteTorus &t1, const DiscreteTorus &t2);
+DiscreteTorus operator*(const uint32_t c, const DiscreteTorus &t1);
 
 #endif
--- src/structure/torus.cpp
+++ src/structure/torus.cpp
@@ -1,65 +1,62 @@
 #include "structure/torus.hpp"
 #include "utility/log.hpp"
 
-namespace galoisfield {
-  DiscreteTorus::DiscreteTorus() : x(0) {};
-  DiscreteTorus::DiscreteTorus(uint32_t x) : x(x) {
-          if (x >= this->P) {
-            Log::error("Discrete Torus Element must be less than P");
-          }
-        };
+DiscreteTorus::DiscreteTorus() : x(0) {};
+DiscreteTorus::DiscreteTorus(uint32_t x) : x(x) {
+        if (x >= this->P) {
+          Log::error("Discrete Torus Element must be less than P");
+        }
+      };
 
-  DiscreteTorus::DiscreteTorus(const DiscreteTorus &t) {
-    this->x = t.x;
-    this->P = t.P;
-    return;
-  };
-
-  uint32_t DiscreteTorus::val() const {
-    return this->x;
-  };
-
-  void DiscreteTorus::operator+=(const DiscreteTorus &t) {
-    uint32_t tmp = this->x + t.x;
-    this->x = modP(tmp);
-    return;
-  };
-
-  void DiscreteTorus::operator-=(const DiscreteTorus &t) {
-    uint32_t tmp = this->x + (this->P - t.x);
-    this->x = modP(tmp);
-    return;
-  };
-
-  void DiscreteTorus::operator*=(const uint32_t c) {
-    uint32_t X = reprMontgomery(this->x);
-    uint32_t C = reprMontgomery(c);
-    this->x = invReprMontgomery(mulMontgomery(X, C));
-    return;
-  };
-
-  uint32_t DiscreteTorus::modP(uint32_t x) {
-    return x%this->P;
-  };
-
-  std::ostream& operator<<(std::ostream &os, const DiscreteTorus &t) {
-    os << t.x;
-    return os;
-  };
-
-
-  DiscreteTorus operator+(const DiscreteTorus &t1, const DiscreteTorus &t2) {
-    DiscreteTorus t = t1;
-    t += t2;
-    return t;
-  }
-
-  DiscreteTorus operator*(const uint32_t c, const DiscreteTorus &t1) {
-    DiscreteTorus t = t1; 
-    t *= c;
-    return t;
-  }
-
+DiscreteTorus::DiscreteTorus(const DiscreteTorus &t) {
+  this->x = t.x;
+  this->P = t.P;
+  return;
 };
 
-galoisfield::DiscreteTorus::DiscreteTorus(const GaloisFieldElement &a) : x(a.val()) {};
+uint32_t DiscreteTorus::val() const {
+  return this->x;
+};
+
+void DiscreteTorus::operator+=(const DiscreteTorus &t) {
+  uint32_t tmp = this->x + t.x;
+  this->x = modP(tmp);
+  return;
+};
+
+void DiscreteTorus::operator-=(const DiscreteTorus &t) {
+  uint32_t tmp = this->x + (this->P - t.x);
+  this->x = modP(tmp);
+  return;
+};
+
+void DiscreteTorus::operator*=(const uint32_t c) {
+  uint32_t X = reprMontgomery(this->x);
+  uint32_t C = reprMontgomery(c);
+  this->x = invReprMontgomery(mulMontgomery(X, C));
+  return;
+};
+
+uint32_t DiscreteTorus::modP(uint32_t x) {
+  return x%this->P;
+};
+
+std::ostream& operator<<(std::ostream &os, const DiscreteTorus &t) {
+  os << t.x;
+  return os;
+};
+
+
+DiscreteTorus operator+(const DiscreteTorus &t1, const DiscreteTorus &t2) {
+  DiscreteTorus t = t1;
+  t += t2;
+  return t;
+}
+
+DiscreteTorus operator*(const uint32_t c, const DiscreteTorus &t1) {
+  DiscreteTorus t = t1; 
+  t *= c;
+  return t;
+}
+
+DiscreteTorus::DiscreteTorus(const GaloisFieldElement &a) : x(a.val()) {};
