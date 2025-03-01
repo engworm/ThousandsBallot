7f4c3e6e194c1c2b844e7602498ffe4e588f413c
engw <morita_1247@yahoo.co.jp>
2025-03-01 21:34:12 +0900

    Add measurment class

---
+++ include/measure/polynomial_multiplication_measure.hpp
@@ -0,0 +1,56 @@
+#ifndef POLYNOMIAL_MULTIPLICATION_MEASURE
+#define POLYNOMIAL_MULTIPLICATION_MEASURE
+
+#include <iostream>
+#include <random>
+#include "structure/intpoly.hpp"
+#include "structure/toruspoly.hpp"
+#include "utility/stopwatch.hpp"
+
+class PolynomialMultiplicationMeasure {
+  private:
+    std::ostream& os;
+    StopWatch<ChronoTimer> sw;
+    std::mt19937 mt;
+    uint32_t total_testcase;
+
+  public:
+    PolynomialMultiplicationMeasure(std::ostream& os, std::mt19937 mt, uint32_t total_testcase) 
+          : os(os), mt(mt), total_testcase(total_testcase) {};
+
+    void measure();
+};
+
+void PolynomialMultiplicationMeasure::measure() {
+
+  std::uniform_int_distribution<uint32_t> dis_poly(0, Params::q-1);
+
+  std::vector<IntPoly> intpolys;
+  intpolys.reserve(total_testcase); 
+  for (int i = 0; i < total_testcase; ++i) {
+      intpolys.emplace_back(Params::N); 
+  }
+
+  std::vector<DiscreteTorusPoly> toruspolys;
+  toruspolys.reserve(total_testcase); 
+  for (int i = 0; i < total_testcase; ++i) {
+      toruspolys.emplace_back(Params::N); 
+  }
+
+  for (int i = 0; i < total_testcase; ++i) {
+    for (size_t j = 0; j < Params::N; ++j) {
+      intpolys[i][j] = dis_poly(mt);
+      toruspolys[i][j] = dis_poly(mt);
+    }
+  }
+
+  this->sw.start("start");
+
+  for (int i = 0; i < total_testcase; ++i) {
+    DiscreteTorusPoly toruspoly = std::move(intpolys[i] * toruspolys[i]);
+  }
+
+  this->sw.stop("stop");
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -4,48 +4,14 @@
 #include <utility>
 #include <fstream>
 
-#include "structure/intpoly.hpp"
-#include "structure/toruspoly.hpp"
-
 #include "utility/parser.hpp"
-#include "utility/stopwatch.hpp"
-#include "utility/log.hpp"
+#include "measure/polynomial_multiplication_measure.hpp"
 
 int main(int argc, char* argv[]) {
   CommandLineParser parser(argc, argv);
 
-  auto prng = parser.PRNG;
-  std::uniform_int_distribution<uint32_t> dis_poly(0, Params::q-1);
-
-
-  std::vector<IntPoly> intpolys;
-  intpolys.reserve(1000); 
-  for (int i = 0; i < 1000; ++i) {
-      intpolys.emplace_back(Params::N); 
-  }
-
-  std::vector<DiscreteTorusPoly> toruspolys;
-  toruspolys.reserve(1000); 
-  for (int i = 0; i < 1000; ++i) {
-      toruspolys.emplace_back(Params::N); 
-  }
-
-  for (int i = 0; i < 1000; ++i) {
-    for (size_t j = 0; j < Params::N; ++j) {
-      intpolys[i][j] = dis_poly(prng);
-      toruspolys[i][j] = dis_poly(prng);
-    }
-  }
-
-  StopWatch<ChronoTimer> sw;
-  sw.start("start");
-
-  for (int i = 0; i < 1000; ++i) {
-    DiscreteTorusPoly toruspoly = std::move(intpolys[i] * toruspolys[i]);
-  }
-  // DiscreteTorusPoly toruspoly = std::move(intpolys[0] * toruspolys[0]);
-  sw.stop("stop");
-  // Log::debug(toruspoly);
+  PolynomialMultiplicationMeasure polynomial_multiplication_measure(std::cout, parser.PRNG, 1000);
+  polynomial_multiplication_measure.measure();
 
   return 0;
 }
